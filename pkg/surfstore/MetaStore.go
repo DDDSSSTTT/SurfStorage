671f14d370254fb8b763291af9ab30ba74ec27ee
DDDSSSTTT <756487617@qq.com>
2022-03-17 12:46:04 -0700

    sync

--- pkg/surfstore/MetaStore.go
+++ pkg/surfstore/MetaStore.go
@@ -19,9 +19,13 @@ func (m *MetaStore) GetFileInfoMap(ctx context.Context, _ *emptypb.Empty) (*File
 
 func (m *MetaStore) UpdateFile(ctx context.Context, fileMetaData *FileMetaData) (*Version, error) {
 	//TODO:TRY TO UPDATE THE FILE OUT HERE
+	log.Printf("Metastore start updating FMDv:%d,mFMMv:%d", fileMetaData.GetVersion(),
+		m.FileMetaMap[fileMetaData.Filename].GetVersion())
 	if fileMetaData.GetVersion() > m.FileMetaMap[fileMetaData.Filename].GetVersion() {
+		log.Printf("Higher Version, push to metamap")
 		m.FileMetaMap[fileMetaData.Filename] = fileMetaData
 	} else {
+		log.Printf("Lower or equal Version, return-1")
 		return &Version{Version: -1}, nil
 	}
 	return &Version{Version: fileMetaData.GetVersion()}, nil
